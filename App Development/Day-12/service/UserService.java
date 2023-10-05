8dffe8756116eabe842e50f0e4f1d8a0fd536e9d
Rasiga Kavin <120013037+RasigaKavin@users.noreply.github.com>
2023-10-05 16:26:17 +0530

    Add files via upload

---
+++ App Development/Day-12/service/UserService.java
@@ -0,0 +1,13 @@
+package com.example.demo.service;
+
+import com.example.demo.model.User;
+
+public interface UserService {
+	
+	    User signup(User user);
+	    User login(User user);
+	    User getUserById(Long id);
+	}
+	
+
+
